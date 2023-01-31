fa120b3a6810638f87440415842b2fe4879272bc
Stoyan Spasov <spasovstoyan@gmail.com>
2023-01-31 15:04:51 +0200

    fix the image

--- src/components/CommingSoon/CommingSoon.jsx
+++ src/components/CommingSoon/CommingSoon.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import headerImage from '../../assets/image0.jpeg'
+import headerImageMobile from '../../assets/image1.jpeg'
 
 import './CommingSoon.css'
 const CommingSoon = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    console.log(window.innerWidth)
+    const getWidth = x => setWidth(x)
+
+    useEffect(() => {
+        window.addEventListener('resize', () => getWidth(window.innerWidth))
+
+        return () => window.removeEventListener('resize', getWidth)
+    }, [width])
+    console.log(width)
     return (
         <div className='comming-soon-wrapper'>
-            <img src={headerImage} alt="Comming soon iamge" className='comming-soon-image' />
+            <img src={width >= 1168 ? headerImage : headerImageMobile} alt="Comming soon iamge" className='comming-soon-image' />
         </div>
     )
 }
